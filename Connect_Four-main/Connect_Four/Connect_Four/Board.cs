3878c29a724766446b0c3d5d8d3857aa4888e772
Edmond1211 <102847600+Edmond1211@users.noreply.github.com>
2022-04-05 12:12:18 +1000

    Add files via upload

---
+++ Connect_Four-main/Connect_Four/Connect_Four/Board.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Connect_Four
+{
+    class Board
+    {
+        const int width = 7;
+        const int height = 6;
+
+        int y = 2;
+        int x = 1;
+        public void Grid()
+        {
+            int[,] myArray = new int[width, height];
+            myArray[x, y] = 5;
+
+            int value = myArray[x, y];
+            WriteLine(value);
+        }
+
+        public static void BoardFrame()
+        {
+            WriteLine("┌───┰────┰───────────────────────┐");
+            WriteLine("┠───┃────┃───────────────────────│");
+            WriteLine("│───────────────────────────────│");
+            WriteLine("│───────────────────────────────│");
+            WriteLine("│───────────────────────────────│");
+            WriteLine("│───────────────────────────────│");
+            WriteLine("└───────────────────────────────┘");
+
+        }
+
+
+
+    }
+}
+                           
